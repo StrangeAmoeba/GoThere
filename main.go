f268fc0933242453f3e6f3f96f15c2ad5abd9a13
SaiHarshaK <k.saiharsha7@gmail.com>
2019-02-18 01:35:30 +0530

    basic web application, fix Locations to support import, basic structure added

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+  "log"
+  "fmt"
+  "net/http"
+  "os"
+  "server"
+)
+
+func determineListenAddress() (string, error) {
+  port := os.Getenv("PORT")
+  if port == "" {
+    return "", fmt.Errorf("$PORT not set")
+  }
+  return ":" + port, nil
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintln(w, "Hello World!!")
+}
+
+func main() {
+  addr, err := determineListenAddress()
+  if err != nil {
+    log.Fatal(err)
+  }
+  http.HandleFunc("/", hello)
+  log.Printf("Listening on %s...\n", addr)
+  if err := http.ListenAndServe(addr, nil); err != nil {
+    panic(err)
+  }
+
+  //testing
+  fmt.Println(server.Locations())
+}
