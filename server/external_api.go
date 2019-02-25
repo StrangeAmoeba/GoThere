6d825abeb81be1ba4856ebaeda686077448e6377
SaiHarshaK <k.saiharsha7@gmail.com>
2019-02-26 01:15:27 +0530

    optimize url constructor, add docs for external_api.go

--- server/external_api.go
+++ server/external_api.go
@@ -5,22 +5,32 @@ import (
   "fmt"
   "io/ioutil"
   "net/http"
+  "strings"
 )
 
+// constructURL takes the origin and destination Vertices which consists of
+// its latitude and longitude. We contruct a url with accordance to google api requirement
+// for parsing the json file which we will receive later on.
+// Input: origin [ origin vertex ] i.e. dt.Vertex, destination [ destination vertex ] i.e. dt.Vertex
+// Output: url [ url link address ] i.e. string
 func constructURL(origin, dest dt.Vertex) string {
-  var url = "https://maps.googleapis.com/maps/api/directions/json?origin="
+  var url strings.Builder
+  fmt.Fprintf(&url, "https://maps.googleapis.com/maps/api/directions/json?origin=")
   // server key
   var key = "AIzaSyDUJjTw3LKudLIxSj_saJQLHsmH_RTfa9w"
   // attach origin co-ordinates to the url
-  url += fmt.Sprint(origin.Lat) + "," + fmt.Sprint(origin.Long) + "&destination="
+  fmt.Fprintf(&url, "%s%s%s%s", fmt.Sprint(origin.Lat), ",", fmt.Sprint(origin.Long), "&destination=")
   // attach origin co-ordinates to the url
-  url += fmt.Sprint(dest.Lat) + "," + fmt.Sprint(dest.Long)
+  fmt.Fprintf(&url, "%s%s%s", fmt.Sprint(dest.Lat), ",", fmt.Sprint(dest.Long))
   // attach the key to the url
-  url += "&key=" + key
-  url += "&departure_time=now&traffic_model=pessimistic"
-  return url
+  fmt.Fprintf(&url, "%s%s%s", "&key=", key, "&departure_time=now&traffic_model=pessimistic")
+  return url.String()
 }
 
+// getResponse is responsible for fetching the content of website which is specified
+// via the url.
+// Input: url [ url link address ] i.e. string
+// Output: html [ html content in bytes ] i.e. []byte
 func getResponse(url string) []byte {
   resp, err := http.Get(url)
   // handle the error if there is one
