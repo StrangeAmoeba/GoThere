b1cdb1b5e34097848c8080eedabce46a7481fbc3
sagar <j.sagar700@gmail.com>
2019-02-28 01:18:12 +0530

    deploy script to be called on termination

--- CD/CDServer/webHookListener.go
+++ CD/CDServer/webHookListener.go
@@ -21,13 +21,17 @@ func restartApp(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Fatal(err)
 	}
+	out, err = exec.Command("deploy-script").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Printf("output is %s\n", out)
 	w.WriteHeader(200)
 }
 func StartServer() {
 	http.HandleFunc("/echo/", echo)
 	http.HandleFunc("/restart", restartApp)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(":1337", nil); err != nil {
 		panic(err)
 	}
 }
