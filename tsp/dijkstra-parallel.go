25f74da183d7875447480e0fbfd7108984d73e47
srinivasboga7 <45848292+srinivasboga7@users.noreply.github.com>
2019-02-28 23:25:09 +0530

    Added comments and functionality

--- tsp/dijkstra-parallel.go
+++ tsp/dijkstra-parallel.go
@@ -1,160 +1,220 @@
 package tsp
 
-// import (
-// 	"math"
-// )
+import (
+	"math"
+	"sync"
+)
 
-// const inf = math.MaxFloat64
+const Inf = math.MaxFloat64
 
-// type node struct {
-// 	index int
-// 	dist  float64
-// 	pred  int
-// 	next  *node
-// }
+type node struct{
+	index int
+	dist float64
+	pred int
+	next *node
+}
 
-// //linked list functions
-// func add(head *node, index int, dist float64, pred int) *node {
-// 	new_node := node{index: index, dist: dist, pred: pred}
-// 	if head == nil {
-// 		head = &new_node
-// 	} else {
-// 		temp := head
-// 		for temp.next != nil {
-// 			temp = temp.next
-// 		}
-// 		temp.next = &new_node
-// 	}
-// 	return head
-// }
+func Add(head *node,index int,dist float64,pred int) (*node){
+	newNode := node{index:index,dist:dist,pred:pred}
+	if head == nil{
+		head = &newNode
+	}else{
+		temp := head
+		for temp.next != nil{
+			temp = temp.next
+		}
+		temp.next = &newNode
+	}
+	return head
+}
 
-// func delete(head *node, min int) *node {
-// 	temp := head
-// 	prev := head
-// 	if head.index == min {
-// 		head = head.next
-// 		return head
-// 	}
-// 	for temp != nil {
-// 		if temp.index == min {
-// 			prev.next = temp.next
-// 			temp.next = nil
-// 			temp = nil
-// 			break
-// 		}
-// 		prev = temp
-// 		temp = temp.next
-// 	}
-// 	return head
-// }
+func Delete(head *node,min int) (*node){
+	temp := head
+	prev := head
+	if head.index == min{
+		head = head.next
+		return head
+	}
+	for temp != nil{
+		if temp.index == min{
+			prev.next = temp.next
+			temp.next = nil
+			temp = nil
+			break
+		}
+		prev = temp
+		temp = temp.next
+	}
+	return head
+}
 
-// func compute_min(mem *node) *node {
-// 	min_node := mem
-// 	temp := mem
-// 	for temp != nil {
-// 		if temp.dist < min_node.dist {
-// 			min_node = temp
-// 		}
-// 		temp = temp.next
-// 	}
-// 	return min_node
-// }
+// ComputeLocalMin is a Helper function for SingleSourceShortestPath function.
+// It returns the minimum of all the local minimas of the given vertices
+// 
+// Input   : A *node, head of list containing all vertices in cluster.
+// Outputs : A *node containing the Local minimum.
+func ComputeLocalMin(mem *node) (*node){
+	minNode := mem
+	temp := mem
+	for temp != nil{
+		if temp.dist < minNode.dist{minNode = temp}
+		temp = temp.next
+	}
+	return minNode
+}
 
-// //function to find the global minimum
-// func find_min(local []*node) *node {
-// 	min_node := &node{dist: inf}
-// 	for _, v := range local {
-// 		if v != nil {
-// 			if min_node.dist > v.dist {
-// 				min_node = v
-// 			}
-// 		}
-// 	}
-// 	return min_node
-// }
+// ComputeGlobalMin is a Helper function for Dijkstra function.
+// It returns the minimum of all the local minimas of the given vertices
+// 
+// Input   : A []*node slice containing all the Local minimum computed parallely
+//			 by Go routines.
+// Outputs : A *node containing the global minimum.
+func ComputeGlobalMin(local []*node) (*node){
+	minNode := &node{index : -1,dist:Inf}
+	for _,v := range local{
+		if v != nil{
+			if minNode.dist > v.dist{
+				minNode = v
+			}
+		}
+	}
+	return minNode
+}
 
-// //computing the local minimum in the cluster of vertices
-// func compute(members *node, source int,
-// 	dest int, ch chan *node, quit chan int, adj_matrix [][]float64) {
-// 	s := source
-// 	var d float64
-// 	d = 0
-// 	for {
-// 		temp := members
-// 		var v float64
-// 		var i int
-// 		for temp != nil {
-// 			v = temp.dist
-// 			i = temp.index
-// 			if adj_matrix[s-1][i-1] != 0 && adj_matrix[s-1][i-1]+d < v {
-// 				temp.dist = adj_matrix[s-1][i-1] + d
-// 				temp.pred = s
-// 			}
-// 			temp = temp.next
-// 		}
-// 		min_node := compute_min(members)
-// 		ch <- min_node
-// 		g_min := <-ch
-// 		cluster = add(cluster, g_min.index, g_min.dist, g_min.pred)
-// 		if min_node != nil {
-// 			if min_node.index == g_min.index {
-// 				members = delete(members, min_node.index)
-// 			}
-// 		}
-// 		if g_min.index == dest || g_min.index == 0 {
-// 			quit <- 0
-// 			break
-// 		}
-// 		s = g_min.index
-// 		d = g_min.dist
-// 	}
-// }
+// ClusterAnalysis function is a Helper function to SingleSourceShortestPath function.
+// Input  : A *node, head of the Linked list containing the nodes of Cluster
+//			A single int ,containing source vertex.
+//			A single int ,containing dest vertex
+//			A chan *node used to send the Local minimum to the main thread
+//			A chan int to send the signal to confirm that go routines exited
+//			A [][]float64 slice, representing the graph
+func ClusterAnalysis(members *node,source int,
+	dest int,ch chan *node,quit chan int,matrix [][] float64){
+	//TODO : can use priority queues instead of linked list to store vertices
+	s := source
+	var d float64
+	d = 0
+	for {
+		temp := members
+		var v float64
+		var i int
+		for temp != nil{
+			v = temp.dist
+			i = temp.index
+			if matrix[s][i] != 0 && matrix[s][i] + d < v{
+				temp.dist = matrix[s][i] + d
+				temp.pred = s
+			}
+			temp = temp.next
+		}
+		minNode := ComputeLocalMin(members)
+		ch <- minNode
+		globalMin := <- ch
+		if minNode != nil{
+			if minNode.index == globalMin.index{
+				members = Delete(members,minNode.index)
+			}
+		}
+		if globalMin.index == dest{
+			quit <- 0
+			break
+		}
+		s = globalMin.index
+		d = globalMin.dist
+	}	
+}
 
-// func singleSourceShortestPath(source int, dest int, adj_matrix [][]float64) (map[int]int, float64) {
-// 	num_partitions = 4
-// 	var mem *node
-// 	ch := make([]chan *node, num_partitions)
-// 	quit := make(chan int)
-// 	members := make([]*node, num_partitions)
-// 	for i = 0; i < num_partitions; i++ {
-// 		ch[i] = make(chan *node)
-// 	}
-// 	for i = 1; i <= V; i++ {
-// 		if i != source {
-// 			members[i%num_partitions] = add(members[i%num_partitions], i, inf, -1)
-// 			mem = add(mem, i, inf, -1)
-// 		}
-// 	}
-// 	for i = 0; i < num_partitions; i++ {
-// 		go compute(members[i], source, dest, ch[i], adj_matrix, quit, preds[i])
-// 	}
-// 	g_min := make([]*node, num_partitions)
-// 	pred = make(map[int]int)
-// 	c := 0
-// 	var m *node
-// 	// computing global minimum
-// 	for {
-// 		for i = 0; i < num_partitions; i++ {
-// 			g_min[i] = <-ch[i]
-// 		}
-// 		m = find_min(g_min)
-// 		pred[m.index] = m.pred
-// 		for i = 0; i < num_partitions; i++ { // broadcasting it to all the nodes
-// 			ch[i] <- m
-// 		}
-// 		if m.index == dest || m.index == 0 {
-// 			break
-// 		}
-// 	}
-// 	for {
-// 		select {
-// 		case <-quit:
-// 			c++
-// 		default:
-// 		}
-// 		if c == 3 {
-// 			break
-// 		}
-// 	}
-// 	return pred, m.dist //returning minimum path and minimum distance for a given source vertex pair
-// }
+// SingleSourceShortestPath is a helper function for Dijkstra function.
+// The function returns the least weight and corresponding path from a source
+// vertex to destination vertex in the graph.
+//
+// Input   : A single int, representing the source vertex
+//		     A single int, representing the destination vertex
+//		     A [][]float64 slice ,representing the Adjacency Matrix of the Graph
+// Outputs : A single float64, containing the minimum distance from source to vertex
+//			 A []int slice containing the minimum path from source to destination
+func SingleSourceShortestPath(source int,dest int,matrix [][]float64) (float64, []int){
+	if(source == dest){
+		var p[] int
+		p = append(p,source)
+		return 0,p	
+	}
+	partitions := 4
+	V := len(matrix)
+	ch := make([]chan *node,partitions)
+	quit := make(chan int)
+	members := make([]*node,partitions)
+	var i int
+	for i = 0 ;i < partitions ; i++{
+		ch[i] = make(chan *node)
+	}
+	for i = 0 ; i < V ;i++{
+		if i != source{
+			members[i%partitions] = Add(members[i%partitions],i,Inf,-1)
+		}
+	}
+	for i = 0 ; i< partitions ; i++{
+		go ClusterAnalysis(members[i],source,dest,ch[i],quit,matrix)
+	}
+	globalMin := make([]*node,partitions)
+	pred := make(map[int]int)
+	pred[source] = -1
+	c := 0
+	var m *node 
+	for {
+		for i = 0 ; i< partitions ;i++{
+			globalMin[i] = <-ch[i]
+		}
+		m = ComputeGlobalMin(globalMin)
+		pred[m.index] = m.pred
+		for i = 0 ; i< partitions ;i++{ 
+			ch[i] <- m
+		}
+		if m.index == dest{
+			break
+		}
+	}
+	for{
+		select{
+		case <- quit:
+			c++
+		default:
+		}
+		if c == partitions{break}
+	}
+	var path[]int
+	pre := dest
+	for{
+		path = append([]int{pre},path...)
+		pre = pred[pre]
+		if pre == -1{break}
+	}
+	return m.dist,path
+}
+
+// Dijkstra parses the graph, and returns the least weight of all paths between any
+// two vertices in the graph
+// Input   : A [][]float64 slice, representing the Adjacency Matrix of the graph
+// Outputs : A [][]float64 slice, containing the minimum path weights between any two vertices
+//			 A [][][]int slice, containing the minimum paths between any two vertices
+func Dijkstra(matrix [][]float64) ([][]float64,[][][]int){
+	var minGraph [][]float64 = make([][]float64, len(matrix))
+	var minPaths [][][]int = make([][][]int,len(matrix))
+	var nodeWg sync.WaitGroup
+
+	nodeWg.Add(len(matrix)*len(matrix))
+	for src := 0 ; src < len(matrix) ;src++{ 
+		minGraph[src] = make([]float64,len(matrix))
+		minPaths[src] = make([][]int,len(matrix))
+		for dest := 0 ;dest < len(matrix) ;dest++{
+			go func(src int,dest int) {
+				var minpath[]int 
+				minGraph[src][dest],minpath = SingleSourceShortestPath(src,dest,matrix)
+				minPaths[src][dest] = minpath
+				nodeWg.Done()
+			}(src,dest)
+		}
+	}
+	nodeWg.Wait()
+	return minGraph,minPaths
+}
